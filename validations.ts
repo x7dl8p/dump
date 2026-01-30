9f2b038f808fbb765e589eb12a7b349fe7c70ce8
Mohammad <148697364+x7dl8p@users.noreply.github.com>
2026-01-30 16:36:50 +0530

    Update validations.ts

--- validations.ts
+++ validations.ts
@@ -1,1 +1,1 @@
-P_FORMAT_CHECK_LIST = ['0987654321', '1234567890'], E_FORMAT_CHECK_LIST = ['hylo.mohammad@gmail.com', 'test@gmail.com']
+P_FORMAT_CHECK_LIST = ['0987654321', '1234567890'], E_FORMAT_CHECK_LIST = ['test000@gmail.com', 'test@gmail.com']
